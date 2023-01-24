5419da0d25bc493deaf414af5cfcdc38733c20ab
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-24 18:41:21 +0530

    [Piyush] added to valid first name start with cap letter and has 3 minimum character

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using UserRegistrationProblem;
+
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to User Registration Problem !!!");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nSelect option to choice ");
+            Console.WriteLine("1.First Name");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter First Name");
+                    string fName = Console.ReadLine();
+                    UserRegistration.ValidateFirstName(fName);
+                    break;
+                default:
+                    flag = false;
+                    Console.WriteLine("Enter the correct option ");
+                    break;
+            }
+        }
     }
 }
