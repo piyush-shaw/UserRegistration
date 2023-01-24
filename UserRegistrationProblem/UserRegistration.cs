460ca37398b8e946bffc482a21efd461a8db5e7c
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-24 18:34:07 +0530

    [Piyush] added to valid first name start with cap letter and has 3 minimum character

---
+++ UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+	public class UserRegistration
+	{
+		//Pattern for checking the first name that start with cap letter and has minimum 3 character
+		public static string firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+		//Method to check the firstname using regex
+		public static void ValidateFirstName(string fName)
+		{
+			if (Regex.IsMatch(fName, firstName))
+			{
+				Console.WriteLine("First Name :"+fName);
+			}
+			else
+			{
+				Console.WriteLine("First alphabet should be capital and has minimum 3 character ");
+			}
+		}
+	}
+}
+
